5c7b8b8f053acd0145fc0aace0a26b5e6260cdce
Irina Ivlieva <ivlieva@haulmont.com>
2021-02-04 08:39:29 +0400

    Part 6: Change the code in order to make the person bean use autowiring of the country bean by type

--- src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
+++ src/test/java/com/luxoft/springioc/lab1/model/HelloWorldTest.java
@@ -26,7 +26,7 @@ public class HelloWorldTest {
 
 	@Test
 	public void testInitPerson() {
-		UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
+		UsualPerson person = (UsualPerson) context.getBean("person2", Person.class);
 
 		System.out.println(person.getCountry() + "-->" + person.getCountry().getA());
 
